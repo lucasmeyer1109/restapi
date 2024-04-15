7467dd09487def347ba09057b99449bd1f651398
Meyer <63262403+lucasmeyer1109@users.noreply.github.com>
2024-04-15 01:19:17 +0000

    -professor+disciplina

---
+++ src/main/java/ps2/restapi/Disciplina.java
@@ -0,0 +1,48 @@
+package ps2.restapi;
+
+public class Disciplina {
+	private long id;
+	private String nome;
+	private String sigla;
+	private String curso;
+    private int semestre;
+		
+	public Disciplina() {}
+	public Disciplina(long id, String n, String s, String c, int sm) {
+		this.id = id;
+		this.nome = n;
+		this.sigla = s;
+        this.curso = c;
+        this.semestre = sm;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla(){
+        return sigla;
+    }
+    public void setSigla(String sigla){
+        this.sigla = sigla;
+    }
+    public String getCurso(){
+        return curso;
+    }
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+    public int getSemestre(){
+        return semestre;
+    }
+    public void setSemestre(int semestre){
+        this.semestre = semestre;
+    }
+}
---
+++ src/main/java/ps2/restapi/DisciplinaController.java
@@ -0,0 +1,69 @@
+package ps2.restapi;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class DisciplinaController {
+	private List<Disciplina> disciplinas;
+
+	public DisciplinaController() {
+		this.disciplinas = new ArrayList<>();
+		disciplinas.add(new Disciplina(1, "Neymar", "S", "Matematica", 3));
+		disciplinas.add(new Disciplina(2, "Thomaz", "C", "Ciencias", 2));
+		disciplinas.add(new Disciplina(3, "Jubileu", "D", "Administração", 4));
+	}
+
+	@GetMapping("/api/disciplinas")
+	Iterable<Disciplina> getDisciplinas() {
+		return this.disciplinas;
+	}
+	
+	@GetMapping("/api/disciplinas/{id}")
+	Optional<Disciplina> getDisciplina(@PathVariable long id) {
+		for (Disciplina d: disciplinas) {
+			if (d.getId() == id) {
+				return Optional.of(d);
+			}
+		}
+		return Optional.empty();
+	}
+	
+	@PostMapping("/api/disciplinas")
+	Disciplina createDisciplina(@RequestBody Disciplina d) {
+		long maxId = 1;
+		for (Disciplina disc: disciplinas) {
+			if (disc.getId() > maxId) {
+				maxId = disc.getId();
+			}
+		}
+		d.setId(maxId+1);
+		disciplinas.add(d);
+		return d;
+	}
+	
+	@PutMapping("/api/disciplinas/{disciplinaId}")
+	Optional<Disciplina> updateDisciplina(@RequestBody Disciplina disciplinaRequest, @PathVariable long disciplinaId) {
+		Optional<Disciplina> opt = this.getDisciplina(disciplinaId);
+		if (opt.isPresent()) {
+			Disciplina disciplina = opt.get();
+			disciplina.setNome(disciplinaRequest.getNome());
+			disciplina.setSigla(disciplinaRequest.getSigla());
+			disciplina.setCurso(disciplinaRequest.getCurso());
+            disciplina.setSemestre(disciplinaRequest.getSemestre());
+		}
+		return opt;				
+	}	
+	
+	@DeleteMapping(value = "/api/disciplinas/{id}")
+	void deleteDisciplina(@PathVariable long id) {
+		disciplinas.removeIf(d -> d.getId() == id);
+	}		
+}
